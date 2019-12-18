74baada80f7a2fe7ae0d0d90e2c6dec24971aef6
Firas Chaabani <firaschaabani1@gmail.com>
2019-12-18 02:47:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array.push ("element5")
+  array.push ("last")
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift ("element")
+  array.unshift ("first")
 end
 
 def remove_element_from_end_of_array(array)
