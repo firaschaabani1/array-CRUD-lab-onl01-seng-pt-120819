e2ecc08d368d40a7b8991439ee7d54237fa32107
Firas Chaabani <firaschaabani1@gmail.com>
2019-12-18 02:44:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array = ["array", "element"]
+  array = ["element""element1", "element2", "element3", ]
 end
 
 def add_element_to_end_of_array(array, element)
